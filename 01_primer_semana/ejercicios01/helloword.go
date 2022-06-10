49556040ac4591148989a748e3af4fa0a99c9c7b
Karen Elpidio <Karen73@live.com.ar>
2022-06-10 16:19:44 -0300

    Ejercicio 1

---
+++ 01_primer_semana/ejercicios01/helloword.go
@@ -0,0 +1,23 @@
+// Ejercicio: En el main imprimir por pantalla un hola mundo.
+
+/**Paquete**/
+package main 
+
+/** Paquetes importados **/
+import "fmt" 
+
+/** Código 1**/
+
+func main(){
+	fmt.Printf("Hello word") // imprimo por consola el "Hello word"
+}
+
+/** codigo 2
+
+func main(){
+	hello := "hola" // Declaro una variable y le asigno un valor strings 
+	fmt.Printf(hello) // Llamo a la variable y la imprimo por consola. 
+}
+
+**/
+
